5ac6a7d53ef0c07298a7a80cebc42ebbe45d6d47
Robert Szlufik <robert.szlufik123@gmail.com>
2023-06-10 14:14:41 +0100

    second test added

--- src/main/java/githubactions/test/demo/MainController.java
+++ src/main/java/githubactions/test/demo/MainController.java
@@ -2,6 +2,7 @@ package githubactions.test.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -9,6 +10,11 @@ public class MainController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/hello")
+    public @ResponseBody String sayHello() {
+        return "Hello, World";
     }
 
 }
--- src/test/java/githubactions/test/demo/ApplicationTests.java
+++ src/test/java/githubactions/test/demo/ApplicationTests.java
@@ -3,10 +3,12 @@ package githubactions.test.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,6 +31,12 @@ class ApplicationTests {
     void shouldReturnIndexPage() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldSayHello() throws Exception {
+        mockMvc.perform(get("/hello")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World")));
     }
 
 }
